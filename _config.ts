ce8fa77bf41642d16d138430f6bc6a43395e13a5
Ohkubo KOHEI <kuboon@trick-with.net>
2022-05-21 00:24:33 +0000

    update lume

--- _config.ts
+++ _config.ts
@@ -1,35 +1,21 @@
 import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
-import bundler from "lume/plugins/bundler.ts";
+import esbuild from "lume/plugins/esbuild.ts";
 import code_highlight from "lume/plugins/code_highlight.ts";
 import inline from "lume/plugins/inline.ts";
 import jsx from "lume/plugins/jsx.ts";
-import { Middleware } from "lume/core.ts";
-
-const github_pages: Middleware = async (req, next) => {
-  const response = await next(req);
-  if(response.status !== 404) {
-    return response;
-  }
-  const req2 = new Request(req.url + ".html", req);
-  return next(req2);
-}
 
 const site = lume({
   src: './pages',
   dest: './docs',
-  prettyUrls: false,
-  server: {
-    middlewares: [github_pages],
-  }
+  prettyUrls: 'no-html-extension'
 });
 site.data('layout', 'layout.njk');
 
-site.use(bundler());
+site.use(esbuild());
 site.use(code_highlight());
 site.use(date());
 site.use(inline());
 site.use(jsx());
-site.copy("_assets", ".");
-
+site.copy("assets", ".");
 export default site;
---
+++ pages/scripts/mod.ts
@@ -0,0 +1,7 @@
+/// <reference lib="dom" />
+
+addEventListener('load', ()=>{
+  Array.from(document.body.children).forEach(el=>{
+    el
+  });
+})
