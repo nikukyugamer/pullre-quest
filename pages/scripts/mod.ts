a52cd5e8ef4128886a35c0426537c377e26305f2
Ohkubo KOHEI <kuboon@trick-with.net>
2022-05-21 02:54:25 +0000

    hash paging

--- pages/scripts/mod.ts
+++ pages/scripts/mod.ts
@@ -1,7 +1,44 @@
 /// <reference lib="dom" />
 
-addEventListener('load', ()=>{
-  Array.from(document.body.children).forEach(el=>{
-    el
+addEventListener("load", () => {
+  generateH1Containers();
+  focusBlock()
+});
+addEventListener("hashchange", focusBlock);
+
+type Block = { id: string; children: HTMLElement[] };
+const blocks: Record<string, HTMLElement> = {};
+
+function generateH1Containers() {
+  let block: Block | undefined = undefined;
+  Array.from(document.body.children).forEach((el) => {
+    if (el.tagName === "H1") {
+      if (block) generateContainer(block);
+      block = { id: el.id, children: [] };
+    }
+    if (block) {
+      block.children.push(el as HTMLElement);
+    }
   });
-})
+  if (block) generateContainer(block);
+}
+function generateContainer(block: Block) {
+  const container = document.createElement("article");
+  container.id = block.id+"-container";
+  block.children[0].before(container);
+  container.append(...block.children);
+  blocks[block.id] = container;
+}
+function focusBlock(){
+  const hash = window.location.hash.slice(1);
+  const decoded = hash ? decodeURIComponent(hash) : "title";
+  if(!blocks[decoded])return
+  for(const key in blocks){
+    const container = blocks[key];
+    if(key === decoded){
+      container.classList.add("active");
+    } else {
+      container.classList.remove("active");
+    }
+  }
+}
